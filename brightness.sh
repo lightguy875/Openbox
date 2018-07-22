59dfd2301398fd127142531920680b81f0e1a422
lightguy875 <luz875@gmail.com>
2018-07-22 00:29:20 -0300

    brightness script created

---
+++ brightness.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##brightness level
+
+##changing black light level
+status=`xbacklight -get`
+if [ "$1" = "up" ]; then
+	xbacklight +5
+elif [ "$1" = "down" ]; then
+	xbacklight -5
+fi
+
+##killing notification
+(pidof notify-osd 1>/dev/null && killall notify-osd)
+
+##No sound to play
+
+##Defining Icon to send
+ICON="notification-display-brightness-full"
+##Definind new xblacklight level
+status_new=`xbacklight -get`
+##sending notification
+notify-send " " -i $ICON -t 5000 -h int:value:$status_new -h string:synchronouse:brightness
