16493e8f20d6f7f01dcb82c8abda67cfff45e774
lightguy875 <luz875@gmail.com>
2018-07-21 22:06:46 -0300

    Volume notification script made

---
+++ volume.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## sound level
+STATUS=`pamixer --get-volume`
+
+## changing sound level
+if [ "$1" = "toggle" ]; then
+	pamixer -t
+elif [ "$1" = "volup" ]; then
+	pamixer -u
+	pamixer -i 5
+elif [ "$1" = "voldown" ]; then
+	pamixer -u 
+	pamixer -d 5
+fi
+
+## playing sound
+##play /usr/share/sounds/freedesktop/stereo/bell.oga -q
+
+## killing existing notifications
+##(pidof xfce4-notifyd 1>/dev/null && killall xfce4-notifyd)
+pkill notify
+## new sound level after change
+STATUS_NEW=`pamixer --get-volume`
+
+## sound muted on/off
+MUTED=`pamixer --get-mute`
+
+## icon based on volume level
+if [ "$STATUS_NEW" -le 33 ]; then
+	ICON="notification-audio-volume-low"
+elif [ "$STATUS_NEW" -ge 66 ]; then
+	ICON="notification-audio-volume-high"
+else
+	ICON="notification-audio-volume-medium"
+fi
+
+if [ "$MUTED" = "true" ]; then
+	ICON="notification-audio-volume-muted"
+fi
+
+## sending notification
+notify-send " " -i $ICON -t 500 -h int:value:$STATUS_NEW -h string:synchronous:volume
