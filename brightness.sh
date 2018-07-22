7049efdb7dc3b6d6a492d64df8d30f2dc401537f
lightguy875 <luz875@gmail.com>
2018-07-22 12:12:48 -0300

    Performance improved

--- brightness.sh
+++ brightness.sh
@@ -4,9 +4,9 @@
 ##changing black light level
 status=`xbacklight -get`
 if [ "$1" = "up" ]; then
-	xbacklight +5
+	xbacklight -inc 5
 elif [ "$1" = "down" ]; then
-	xbacklight -5
+	xbacklight -dec 5
 fi
 
 ##killing notification
